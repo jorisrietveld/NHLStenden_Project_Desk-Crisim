b7eb07fa033891e21fa34045373e03435763a58e
Joris Rietveld <jorisrietveld@gmail.com>
2018-11-20 01:13:00 +0100

    Added first templates, encore, js, css and translations.

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,9 +6,9 @@
  */
 
 // any CSS you require will output into a single css file (app.css in this case)
-require('../css/app.css');
+require("../scss/app.scss");
 
-// Need jQuery? Install it with "yarn add jquery", then uncomment to require it.
-// var $ = require('jquery');
+// Setup an global variable for jQuery.
+let $ = require('jquery');
+require("jquery.easing");
 
-console.log('Hello Webpack Encore! Edit me in assets/js/app.js');
