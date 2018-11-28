d6aebcaafcfa8d727574daf61a398b420db64ea3
Sander111 <sanderpaping@live.nl>
2018-11-28 15:35:58 +0100

    statistics toegevoegd werkend

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -20,6 +20,45 @@ $(document).ready(function () {
     $('[data-toggle="tooltip"]').tooltip();
 });
 
+
+$(document).ready(function () {
+    $('select').formSelect();
+});
+
+
+
+var ctx = document.getElementById("myChart").getContext('2d');
+var myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: ["Activeren", "Negeren", "Reageren"],
+        datasets: [{
+            label: "Number of votes",
+            data: [6, 12, 4],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255,99,132,1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true
+                }
+            }]
+        }
+    }
+});
+
 /* todo figure out which one to use.
 $(document).ready(function () {
     $('.tooltipped').tooltip();
